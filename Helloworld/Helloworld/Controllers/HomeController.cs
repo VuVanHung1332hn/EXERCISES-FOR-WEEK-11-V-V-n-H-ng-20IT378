71c7ea9cabc911b698649460c63db84d26554b26
VuVanHung1332hn <crushlak7.0@gmail.com>
2023-04-17 21:27:51 +0700

    first commit

---
+++ Helloworld/Helloworld/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Helloworld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GotoHome()
+        {
+            return View();
+        }
+    }
+}
